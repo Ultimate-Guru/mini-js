93dc9cac6c18f1039fc0825589451cfeb35bbaca
Ultimate Guru <tosindaniel360@gmail.com>
2025-04-14 11:18:41 +0100

    Fix button onclick handlers to use setColor function and correct JavaScript syntax errors

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const body = document.getElementByTagName("body")[0];
+const body = document.getElementsByTagName("body")[0]
 
-const changeColor = (name) => {
-    body.style.backgorundColor = name;
+function setColor(name) {
+    body.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.round(Math.random() * 255) 
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+    
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color;
 }
